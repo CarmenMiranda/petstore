018fa470c63105a89254d7f9b4b0004fc2ff819e
Carmen Miranda <carmen@vauxoo.com>
2019-09-19 16:32:34 -0500

    [ADD] oepetstore: Add Client Action and Widget

--- oepetstore/static/src/js/petstore.js
+++ oepetstore/static/src/js/petstore.js
@@ -1,20 +1,32 @@
-odoo.define("oepetstore", function(require, local){
+odoo.define("oepetstore", function(require){
     'use strict';
 
     var translation = require('web.translation'),
         core = require('web.core');
     const _t = translation._t,
         QWeb = core.qweb,
-        Widget = require('web.Widget');
+        Widget = require('web.Widget'),
+        AbstractAction = require('web.AbstractAction');
 
 
     // console.log("pet store home page loaded");
-    local.HomePage = Widget.extend({
-        // template: 'website.homepage',
+    var HomePageClientAction =  AbstractAction.extend({
+        className: 'oe_petstore_homepage',
         start: function() {
-            console.log("pet store home page loaded");
+            // console.log("pet store home page loaded");
+            this.$el.append("<div>Hello dear Odoo user!</div>");
+            var greeting = new GreetingsWidget(this);
+            greeting.appendTo(this.$el);
+            // console.log(this.getChildren()[0].$el);
         },
     });
+    core.action_registry.add('petstore-homepage', HomePageClientAction);
 
-    // instance.web.client_actions.add('petstore.homepage', 'instance.oepetstore.HomePage');
+    var GreetingsWidget = Widget.extend({
+        className: 'oe_petstore_greetings',
+        start: function() {
+            this.$el.append("<div>We are so happy to see you again in this menu!</div>");
+            // console.log(this.getParent().$el);
+        },
+    });
 });
