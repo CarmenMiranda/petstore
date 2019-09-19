dd0d48f2290bac2f97e86468ae21dedfd8978d99
Carmen Miranda <carmen@vauxoo.com>
2019-09-19 17:26:18 -0500

    [ADD] oepetstore: Using QWeb Template Engine

--- oepetstore/static/src/js/petstore.js
+++ oepetstore/static/src/js/petstore.js
@@ -13,13 +13,28 @@ odoo.define("oepetstore", function(require){
     var HomePageClientAction =  AbstractAction.extend({
         className: 'oe_petstore_homepage',
         start: function() {
-            // console.log("pet store home page loaded");
-            this.$el.append("<div>Hello dear Odoo user!</div>");
-            var greeting = new GreetingsWidget(this);
-            greeting.appendTo(this.$el);
+            // this.$el.append("<div>Hello dear Odoo user!</div>");
+            // var greeting = new GreetingsWidget(this);
+            // greeting.appendTo(this.$el);
             // console.log(this.getChildren()[0].$el);
+
+            // First way to use a template
+            // this.$el.append(QWeb.render("HomePageTemplate", {name: "Klaus"}));
+            var products = new ProductsWidget(this,
+                ['cpu', 'mouse', 'keyboard', 'graphic card', 'screen'], "#00FF00");
+            products.appendTo(this.$el);
         },
     });
+
+    // Second way to use a template
+    // var HomePageClientAction =  AbstractAction.extend({
+    //     template: "HomePageTemplateWidget",
+    //     xmlDependencies: ['static/src/xml/petstore.xml'],
+    //     init: function(parent) {
+    //         this._super(parent);
+    //         this.name = "Mordecai";
+    //     },
+    // });
     core.action_registry.add('petstore-homepage', HomePageClientAction);
 
     var GreetingsWidget = Widget.extend({
@@ -27,6 +42,15 @@ odoo.define("oepetstore", function(require){
         start: function() {
             this.$el.append("<div>We are so happy to see you again in this menu!</div>");
             // console.log(this.getParent().$el);
+        },
+    });
+
+    var ProductsWidget = Widget.extend({
+        template: "ProductsWidget",
+        init: function(parent, products, color) {
+            this._super(parent);
+            this.products = products;
+            this.color = color;
         },
     });
 });
